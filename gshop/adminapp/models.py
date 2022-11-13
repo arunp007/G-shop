5ccbac454435e08f33e6e0772c80f0ddb6b92f62
arunp007 <arunp4624@gmail.com>
2022-11-13 21:38:55 +0530

    Added Some Changes

--- gshop/adminapp/models.py
+++ gshop/adminapp/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 class Grocery(models.Model):
-    name = models.TextField(max_length=50)
-    description = models.TextField(max_length=50)
-    price = models.TextField(max_length=50)
-    image = models.CharField(max_length=100)
+    name = models.TextField(max_length=100)
+    description = models.TextField(max_length=100)
+    price = models.TextField(max_length=100)
+    image = models.CharField(max_length=150)
 
     class Meta:
         db_table = 'grocery'
 
 class Medicine(models.Model):
-    name = models.TextField(max_length=50)
-    description = models.TextField(max_length=50)
-    price = models.TextField(max_length=50)
-    image = models.TextField(max_length=100)
+    name = models.TextField(max_length=100)
+    description = models.TextField(max_length=100)
+    price = models.TextField(max_length=100)
+    image = models.TextField(max_length=150)
 
     class Meta:
         db_table = 'medicine'
