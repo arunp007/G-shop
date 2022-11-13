71536d22a4cd7be9b1c87470cec221538dae9d48
arunp007 <arunp4624@gmail.com>
2022-11-13 15:16:45 +0530

    Added Some Changes

--- gshop/adminapp/urls.py
+++ gshop/adminapp/urls.py
@@ -4,5 +4,13 @@ from.import views
 urlpatterns = [ 
     path('admin_home', views.admin_home, name = 'admin_home'),
     path('grocery_add', views.grocery_add, name = 'grocery_add'),
+    path('grocery_table', views.grocery_table, name = 'grocery_table'),
+    path('grocery_update/<int:id>', views.grocery_update, name = 'grocery_update'),
+    path('grocery_delete/<int:id>', views.grocery_delete, name = 'grocery_delete'),
     path('medicine_add', views.medicine_add, name = 'medicine_add'),
+    path('medicine_table', views.medicine_table, name = 'medicine_table'),
+    path('medicine_delete/<int:id>', views.medicine_delete, name = 'medicine_delete'),
+    path('medicine_update/<int:id>', views.medicine_update, name = 'medicine_update'),
+    path('grocery', views.grocery, name = 'grocery'),
+    path('medicine', views.medicine, name = 'medicine'),
 ]
--- gshop/adminapp/views.py
+++ gshop/adminapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from random import random
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
@@ -21,6 +21,28 @@ def grocery_add(request):
         object_details.save()
     return render(request, 'grocery_add.html')
 
+def grocery_table(request):
+    grocery_data = Grocery.objects.all()
+    return render(request, 'grocery_table.html', {'grocery': grocery_data})
+
+def grocery_update(request,id):
+    update_data = ""
+    if request.method == 'POST':
+       name = request.POST['gname']
+       description = request.POST['description']
+       price = request.POST['price']
+       Grocery.objects.filter(id=id).update(name = name, description = description, price = price)
+       return redirect('grocery_table')
+
+    else:
+        update_data = Grocery.objects.get(id=id)
+        return render(request, 'grocery_add.html', {'update': update_data})
+    return render(request, 'grocery_add.html')
+
+def grocery_delete(request,id):
+    Grocery.objects.get(id=id).delete()
+    return redirect('grocery_table')
+
 def medicine_add(request):
     if request.method == 'POST':
         name = request.POST['mname']
@@ -34,3 +56,33 @@ def medicine_add(request):
         object_details = Medicine(name = name, description = description, price = price, image = image)
         object_details.save()
     return render(request, 'medicine_add.html')
+
+def medicine_table(request):
+    medicine_data = Medicine.objects.all()
+    return render(request, 'medicine_table.html',{'medicine': medicine_data})
+
+def medicine_delete(request,id):
+    Medicine.objects.get(id=id).delete()
+    return redirect('medicine_table')
+
+def medicine_update(request,id):
+    update_data = ""
+    if request.method == 'POST':
+        name = request.POST['mname']
+        description = request.POST['mdescription']
+        price = request.POST['mprice']
+        Medicine.objects.filter(id=id).update(name = name, description = description, price = price)
+        return redirect('medicine_table')
+
+    else:
+        update_data = Medicine.objects.get(id=id)
+        return render(request, 'medicine_add.html', {'update': update_data})
+    return render(request, 'medicine_add.html')
+
+def grocery(request):
+    grocery_data = Grocery.objects.all()
+    return render(request, 'grocery.html', {'grocery': grocery_data})
+
+def medicine(request):
+    medicine_data = Medicine.objects.all()
+    return render(request, 'medicine.html', {'medicine': medicine_data})
